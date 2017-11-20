0fed6268051ac6d81c8b3f8e704b93195d933d67
Namila Bandara <namila007@gmail.com>
2017-11-21 02:06:11 +0530

    added database with Apache ANT integration

--- Database/data/userdata.sql
+++ Database/data/userdata.sql
@@ -1,1 +1,1 @@
-INSERT INTO `tbl_user` (`id`, `name`, `age`, `other`) VALUES ('1', 'namila', '24', 'this is a test');
+INSERT INTO `tbl_user` (`id`, `name`, `age`, `other`) VALUES ('1', 'namila', '24', 'this is a test run');
